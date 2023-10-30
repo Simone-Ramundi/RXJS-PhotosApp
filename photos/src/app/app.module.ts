82f24048c538f3efb0241131ca3efcc15d9cc726
Simone Ramundi <simone.ramundi@4science.com>
2023-10-30 12:15:47 +0100

    Added HTTPCLIENTMODULE and used dependency injection to get access to HTTPCLIENT

--- photos/src/app/app.module.ts
+++ photos/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { PhotoShowComponent } from './photo-show/photo-show.component';
 
@@ -10,7 +10,8 @@ import { PhotoShowComponent } from './photo-show/photo-show.component';
     PhotoShowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
