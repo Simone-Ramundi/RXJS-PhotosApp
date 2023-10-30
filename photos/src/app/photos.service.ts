140902d892c398eee74be35278710db85b82d792
Simone Ramundi <simone.ramundi@4science.com>
2023-10-30 12:40:04 +0100

    Added new method getPhoto and returning the endpoint + passing key via header object

--- photos/src/app/photos.service.ts
+++ photos/src/app/photos.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+    headers: {
+      Authorization: 'Client-ID YpAwhep7EM__aJ36aJ-glLDqWj4oeVClru8v_cVeFlo'
+    }
+    });
   }
 }
